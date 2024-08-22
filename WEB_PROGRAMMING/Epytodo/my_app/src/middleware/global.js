df162711cd1b78d1e567d4252bb9d3753a33e8ba
Wilson-Epitech <wilson.bordichon@epitech.eu>
2024-08-22 14:15:49 +0200

    chore: remove usless directory

--- WEB_PROGRAMMING/Epytodo/my_app/src/middleware/global.js
+++
@@ -1,9 +0,0 @@
-let loggedUserEmail = '';
-let loggedUserId = '';
-
-module.exports = {
-  getLoggedUserEmail: () => loggedUserEmail,
-  setLoggedUserEmail: (email) => { loggedUserEmail = email; },
-  getLoggedUserId: () => loggedUserId,
-  setLoggedUserId: (id) => { loggedUserId = id; },
-};
